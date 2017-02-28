2bbf47dfec97b31c75df3d5ad416e15068073464
Lucas Andreusi <lucasandreussi@gmail.com>
2017-02-28 02:26:48 -0300

    testing without music

--- src/android/MediaControl.java
+++ src/android/MediaControl.java
@@ -24,59 +24,59 @@ public class MediaControl extends CordovaPlugin {
           PermissionHelper.requestPermission(this, MODIFY_AUDIO_SETTINGS, permissions[MODIFY_AUDIO_SETTINGS]);
           this.callback = callbackContext;
           if (value.equals("play")){
-            if (am.isMusicActive()){
+            // if (am.isMusicActive()){
               am.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY));
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else if (value.equals("pause")){
-            if (am.isMusicActive()){
+            // if (am.isMusicActive()){
               am.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PAUSE));
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else if (value.equals("next")){
-            if (am.isMusicActive()){
+            // if (am.isMusicActive()){
               am.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_NEXT));
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else if (value.equals("prev")){
-            if (am.isMusicActive()){
+            // if (am.isMusicActive()){
               am.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PREVIOUS));
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else if (value.equals("stop")){
-            if (am.isMusicActive()){
+            // if (am.isMusicActive()){
               am.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_STOP));
               fireEvent("success");
-              return true;
+              // return true;
             }
           }
           else if(value.equals("volume+")){
             int currentVolume = am.getStreamVolume(am.STREAM_MUSIC);
             int maxVolume = am.getStreamMaxVolume(am.STREAM_MUSIC);
-            if (am.isMusicActive() && currentVolume <= maxVolume - 10){
+            // if (am.isMusicActive() && currentVolume <= maxVolume - 10){
               currentVolume += 10;
               am.setStreamVolume(am.STREAM_MUSIC, currentVolume, 1);
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else if(value.equals("volume-")){
             int currentVolume = am.getStreamVolume(am.STREAM_MUSIC);
             int maxVolume = am.getStreamMaxVolume(am.STREAM_MUSIC);
-            if (am.isMusicActive() && currentVolume >= 10){
+            // if (am.isMusicActive() && currentVolume >= 10){
               currentVolume -= 10;
               am.setStreamVolume(am.STREAM_MUSIC, currentVolume, 1);
               fireEvent("success");
               return true;
-            }
+            // }
           }
           else{
             fireEvent("error");
